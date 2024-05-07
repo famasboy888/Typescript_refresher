1ee73d91c20204be4ceb380e3ed8270847f03714
famasboy888 <famasboy88@gmail.com>
2024-05-07 14:52:20 +0800

    Added actuals

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,67 @@
-export {};
+let code:number =123_245_789;
 
-let age:number =20;
+console.log(code);
 
-if(age <30){
-    age += 2;
+let arryNum: [number, string, object] = [
+    1,
+    'Batman',
+    { description: 'dark Knight' }
+];
+
+arryNum.forEach(n => { console.log(n) });
+
+const enum Size {
+    Small = 'S',
+    Medium = 'M',
+    Large = 'L'
 }
 
-console.log(age);
+let mySize: Size = Size.Medium
+console.log(mySize);
+
+
+const multiplyByTwo = (value: number, name: string = 'John doe'): number => {
+    if (name) {
+        console.log(name)
+    }
+    return value * 2;
+}
+console.log(multiplyByTwo(2));
+console.log(multiplyByTwo(2, 'Batman'));
+
+const kgToLbs = (weight: number | string): number => {
+    //Narrowing
+    if (typeof weight === 'number') {
+        return weight * 2.2;
+    } else {
+        return parseInt(weight) * 2.2;
+    }
+}
+
+console.log(kgToLbs(10));
+console.log(kgToLbs('22'));
+
+type Draggable = {
+    drag: () => void
+}
+
+type Resizable = {
+    resize: () => void
+}
+
+const UiWidget: Draggable & Resizable = {
+    drag() { },
+    resize() { },
+}
+
+
+type Customer = {
+    birthday: Date
+}
+
+const getNumber = (num: number): Customer | null | undefined => {
+    return num?{birthday:new Date}:null;
+}
+
+const cust = getNumber(1); 
+console.log(cust?.birthday);
